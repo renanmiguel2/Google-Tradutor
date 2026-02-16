51e21239e786b2da3998715f720b222b97c76e0d
Renan Miguel <renanmiguel19900@gmail.com>
2026-02-16 16:48:35 -0300

    Header feito
    Fiz o header e fiz alguns presets de cores no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 function App() {
   return (
-    <div>
-      <h1>Google Tradutor</h1>
+    <div className='min-h-screen bg-background flex flex-col'>
+      <header className='bg-white shadow-sm'>
+        <div className='max-w-5xl mx-auto px-4 py-3 flex items-center'>
+          <h1 className='text-headerColor text-2xl font-normal'>Google Tradutor</h1>
+        </div>
+      </header>
     </div>
   )
 }
